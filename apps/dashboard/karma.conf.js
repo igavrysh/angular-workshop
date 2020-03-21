d3123cb779956b54621bf281443df89e6a76b84a
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-03-21 08:29:51 -0700

    Fox karma config

--- apps/dashboard/karma.conf.js
+++ apps/dashboard/karma.conf.js
@@ -1,7 +1,41 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+/*
+const { join } = require('path');
+const { constants } = require('karma');
+
+module.exports = () => {
+  return {
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: join(__dirname, '../../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: constants.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  };
+};
+*/
 
 const { join } = require('path');
+
 const getBaseKarmaConfig = require('../../karma.conf');
 
 module.exports = function(config) {
@@ -11,6 +45,10 @@ module.exports = function(config) {
     coverageIstanbulReporter: {
       ...baseConfig.coverageIstanbulReporter,
       dir: join(__dirname, '../../coverage/apps/dashboard')
-    }
+    },
+    client: {
+      clearContext: false
+    },
+    singleRun: false
   });
 };
